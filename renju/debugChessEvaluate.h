c814a7a5c2caf2f1fccd71a8784d5e7a3a080c8e
Starrah <46821571+Starrah@users.noreply.github.com>
2020-04-15 12:02:08 +0800

    棋盘估值调试工具在debugChessEvaluate.cpp中

---
+++ renju/debugChessEvaluate.h
@@ -0,0 +1,9 @@
+#ifndef RENJU_DEBUGCHESSEVALUATE_H
+#define RENJU_DEBUGCHESSEVALUATE_H
+
+#include <vector>
+#include "define.h"
+
+void DebugEvaluate();
+
+#endif //RENJU_DEBUGCHESSEVALUATE_H
--- renju/start.cpp
+++ renju/start.cpp
@@ -1,9 +1,9 @@
 #include "define.h"
-#include "searchmove.h"
 #include "printchessboard.h"
 #include "makemove.h"
-
+#include "debugChessEvaluate.h"
 int main() {
+    DebugEvaluate();
     cout << "*************五子棋人机对弈AI*************" << endl;
     cout << "输入: newblack  电脑先手" << endl;
     cout << "输入: newwhite  电脑后手" << endl;
